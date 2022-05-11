a1816a286e9160c4d2d1e3e42c67b2540d3d63a0
Admin <sgoshaa@yandex.ru>
2022-05-11 16:38:48 +0500

    отключил security

--- domain/src/main/java/com/bell/bellschooll/model/Role.java
+++ domain/src/main/java/com/bell/bellschooll/model/Role.java
@@ -1,25 +1,25 @@
-package com.bell.bellschooll.model;
-
-import lombok.Getter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.Set;
-import java.util.stream.Collectors;
-
-@Getter
-public enum Role {
-    USER(Set.of(Permission.USER)),
-    ADMIN(Set.of(Permission.MODERATE, Permission.USER));
-
-    private final Set<Permission> permissions;
-
-    Role(Set<Permission> permissions) {
-        this.permissions = permissions;
-    }
-
-    public Set<SimpleGrantedAuthority> getAuthorities() {
-        return permissions.stream()
-                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
-                .collect(Collectors.toSet());
-    }
-}
+//package com.bell.bellschooll.model;
+//
+//import lombok.Getter;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//
+//import java.util.Set;
+//import java.util.stream.Collectors;
+//
+//@Getter
+//public enum Role {
+//    USER(Set.of(Permission.USER)),
+//    ADMIN(Set.of(Permission.MODERATE, Permission.USER));
+//
+//    private final Set<Permission> permissions;
+//
+//    Role(Set<Permission> permissions) {
+//        this.permissions = permissions;
+//    }
+//
+//    public Set<SimpleGrantedAuthority> getAuthorities() {
+//        return permissions.stream()
+//                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+//                .collect(Collectors.toSet());
+//    }
+//}
--- serviceLayer/src/main/java/com/bell/bellschooll/config/SecurityConfig.java
+++ serviceLayer/src/main/java/com/bell/bellschooll/config/SecurityConfig.java
@@ -1,56 +1,56 @@
-package com.bell.bellschooll.config;
-
-import com.bell.bellschooll.model.Role;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("api/countries").permitAll()
-                .antMatchers("/api/user/list*").hasAuthority(Role.ADMIN.toString())
-                .antMatchers("/api/user").hasAuthority(Role.USER.toString())
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin().permitAll()
-                .and()
-                .httpBasic();
-    }
-
-    @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager(
-                User.builder()
-                        .username("user")
-                        .password(passwordEncoder().encode("user"))
-                        .authorities(Role.USER.getAuthorities())
-                        .build(),
-                User.builder()
-                        .username("admin")
-                        .password(passwordEncoder().encode("admin"))
-                        .authorities(Role.ADMIN.getAuthorities())
-                        .build()
-        );
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
-    }
-}
+//package com.bell.bellschooll.config;
+//
+//import com.bell.bellschooll.model.Role;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("api/countries").permitAll()
+//                .antMatchers("/api/user/list*").hasAuthority(Role.ADMIN.toString())
+//                .antMatchers("/api/user").hasAuthority(Role.USER.toString())
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin().permitAll()
+//                .and()
+//                .httpBasic();
+//    }
+//
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        return new InMemoryUserDetailsManager(
+//                User.builder()
+//                        .username("user")
+//                        .password(passwordEncoder().encode("user"))
+//                        .authorities(Role.USER.getAuthorities())
+//                        .build(),
+//                User.builder()
+//                        .username("admin")
+//                        .password(passwordEncoder().encode("admin"))
+//                        .authorities(Role.ADMIN.getAuthorities())
+//                        .build()
+//        );
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder(12);
+//    }
+//}
